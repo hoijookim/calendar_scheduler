6a5eb46d78dcc6f6f6dceb31e731301b4d71eef0
Hoijoo Kim <hoijoo@firstcorea.com>
2022-10-27 21:49:38 +0900

    Update: Code Generation

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -12,22 +12,36 @@ import 'package:path_provider/path_provider.dart';
 part 'drift_database.g.dart'; // import보다 넓은 기능
 
 @DriftDatabase(
-  tables: [ // database 테이블로 인식 -> type선언 식
+  tables: [
+    // database 테이블로 인식 -> type선언 식
     Schedules,
-    CategoryColors,
+    CategoryColors, D
   ],
 )
 // 클래스 상속 받음 _$LocalDatabase -> drift가 만들어주는 클래스 -> drift_database.g.dart에 생성됨
 class LocalDatabase extends _$LocalDatabase {
-  LocalDatabase(): super(_openConnection());
+  LocalDatabase() : super(_openConnection());
+
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data); // id를 리턴받을 수 있음
+
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+  Future<List<CategoryColor>> getCategoryColors() =>
+      select(categoryColors).get(); // get() 모든 값 가져오기
+
+기 @override
+  int get schemaVersion => 1; // 테이블 상태의 버전
 }
 
 LazyDatabase _openConnection() {
   // database파일을 어떤 위치에 생성해 줄 건지 설정
-  return LazyDatabase(() async{
+  return LazyDatabase(() async {
     // 이 위치는 앱을 보통 설치하면 os에서 앱별 각각 사용 가능한 하드드라이브 특정위치를 지정해줌
     final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'db.sqlite')); // database를 저장할 파일을 생성
+    final file =
+        File(p.join(dbFolder.path, 'db.sqlite')); // database를 저장할 파일을 생성
     return NativeDatabase(file); // file로 데이터베이스 생성
   });
 }
