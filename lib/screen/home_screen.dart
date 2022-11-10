3f521faa0c04568eaf3c27f7e07595f7c4c6203c
Hoijoo Kim <hoijoo@firstcorea.com>
2022-11-10 17:04:36 +0900

    Update: database state

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -53,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
             isScrollControlled: true,
             context: context,
             builder: (_) {
-              return ScheduleBottomSheet();
+              return ScheduleBottomSheet(selectedDate: selectedDay,);
             });
       },
       child: Icon(Icons.add),
